33452adbbe86b393c4613d66cba243495ebff654
cunxinshuihua <cunxinshuihua@gmail.com>
2017-05-31 22:06:03 +0800

    忘记了个文件，重要git add src/!

---
+++ src/util/StaticValue.java
@@ -0,0 +1,21 @@
+package util;
+
+public class StaticValue {
+
+	public static String OUT_DIR = "forIndex/";
+	
+	// lucene
+	public static String INDEX_DIR = "/index/";
+	public static String GLOBAL_PATH = "/global.txt";
+	public static String FILE_LIST_PATH = "/filelist.txt";
+	
+	// page rank
+	public static String PR_PATH = "/pagerank.txt";
+	public static String PR_ANALYZE_PATH = "/analyze.txt";
+	public static String PR_ROUND_INT_PATH = "/rankToInt.txt";
+	
+	// data mining
+	public static String SIM_WORD_PATH = "/relation.txt";
+	public static String AUTO_COM_PATH = "/autocom.txt";
+	public static Integer AUTO_COM_THRESHOLD = 200;
+}
